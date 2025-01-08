cf9c9581c82cff32a1fb6eabbf76325abb2561b4
Kevin Yu <kevin@2046inc.xyz>
2025-01-08 18:48:33 +0800

    fix minor page table errors

--- src/memory/mod.rs
+++ src/memory/mod.rs
@@ -4,7 +4,7 @@ use bootloader::bootinfo::{MemoryMap, MemoryRegionType};
 use x86_64::{
     structures::paging::{
         PageTable, PageTableFlags, PhysFrame, Size4KiB, FrameAllocator,
-        Mapper, Page, RecursivePageTable, OffsetPageTable,
+        Mapper, Page, OffsetPageTable, Translate,
         mapper::MapToError,
     },
     VirtAddr, PhysAddr,
@@ -16,7 +16,7 @@ const PROGRAM_BASE: u64 = 0x400000;
 
 #[derive(Debug)]
 pub struct MemorySpace {
-    page_table: RecursivePageTable<'static>,
+    page_table: OffsetPageTable<'static>,
     heap_start: VirtAddr,
     heap_size: usize,
     code_start: VirtAddr,
@@ -28,37 +28,19 @@ impl MemorySpace {
         let mut guard = FRAME_ALLOCATOR.lock();
         let frame_allocator = guard.as_mut().unwrap();
         
-        // Allocate a new page table
-        let page_table_frame = frame_allocator.allocate_frame()
-            .ok_or("Failed to allocate frame for page table")?;
+        // Create new page table using the physical memory offset
+        let page_table = unsafe {
+            let level_4_table = active_level_4_table(VirtAddr::new(0xffff_8000_0000_0000));
+            OffsetPageTable::new(level_4_table, VirtAddr::new(0xffff_8000_0000_0000))
+        };
             
-        // Get the physical address of the page table
-        let phys_addr = page_table_frame.start_address();
-        
-        // Map it recursively
-        let recursive_index = 511;
-        let recursive_addr = VirtAddr::new(0xffff_ffff_ffff_f000);
-        
-        unsafe {
-            let page_table = &mut *(phys_addr.as_u64() as *mut PageTable);
-            page_table[recursive_index].set_frame(
-                page_table_frame.clone(),
-                PageTableFlags::PRESENT | PageTableFlags::WRITABLE,
-            );
-            
-            // Create recursive page table
-            let page_table = RecursivePageTable::new(
-                &mut *(recursive_addr.as_mut_ptr() as *mut PageTable)
-            ).map_err(|_| "Failed to create recursive page table")?;
-            
-            Ok(Self {
-                page_table,
-                heap_start: VirtAddr::new(0x4000_0000_0000),
-                heap_size: 1024 * 1024, // 1MB heap
-                code_start: VirtAddr::new(0x0000_0000_0000),
-                code_size: 1024 * 1024, // 1MB code segment
-            })
-        }
+        Ok(Self {
+            page_table,
+            heap_start: VirtAddr::new(0x4000_0000_0000),
+            heap_size: 1024 * 1024, // 1MB heap
+            code_start: VirtAddr::new(0x0000_0000_0000),
+            code_size: 1024 * 1024, // 1MB code segment
+        })
     }
 
     pub fn load_program(&mut self, program: &[u8]) -> Result<(), &'static str> {
@@ -66,23 +48,25 @@ impl MemorySpace {
         let frame_allocator = guard.as_mut().unwrap();
         
         let num_pages = (program.len() + PAGE_SIZE - 1) / PAGE_SIZE;
-        let start_page = VirtAddr::new(PROGRAM_BASE).as_mut_ptr::<u8>();
-
+        
         for i in 0..num_pages {
-            let page = unsafe { Page::containing_address(VirtAddr::from_ptr(start_page.add(i * PAGE_SIZE))) };
+            let page_addr = VirtAddr::new(PROGRAM_BASE + (i * PAGE_SIZE) as u64);
+            let page = Page::<Size4KiB>::containing_address(page_addr);
             let frame = frame_allocator.allocate_frame()
                 .ok_or("Failed to allocate frame for program")?;
             let flags = PageTableFlags::PRESENT | PageTableFlags::WRITABLE | PageTableFlags::USER_ACCESSIBLE;
             
             unsafe {
                 self.page_table
-                    .map_to(page, frame, flags, frame_allocator)
+                    .map_to(page, frame.clone(), flags, frame_allocator)
                     .map_err(|_| "Failed to map page")?
                     .flush();
 
                 let start = i * PAGE_SIZE;
                 let end = core::cmp::min((i + 1) * PAGE_SIZE, program.len());
-                let dest = frame.start_address().as_u64() as *mut u8;
+                let dest = self.page_table.translate_addr(page_addr)
+                    .ok_or("Failed to translate virtual address")?
+                    .as_u64() as *mut u8;
                 core::ptr::copy_nonoverlapping(
                     program[start..end].as_ptr(),
                     dest,
@@ -94,7 +78,7 @@ impl MemorySpace {
     }
 
     pub fn entry_point(&self) -> usize {
-        self.code_start.as_u64() as usize
+        PROGRAM_BASE as usize
     }
 }
 
